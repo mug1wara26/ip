da0c93d1c68cd1d99ae97a280d115128935d2660
mug1wara26 <aloysiusgsq@gmail.com>
2025-08-16 21:52:28 +0800

    Implemented Level 1, added .class to gitignore

--- src/main/java/Lmbd.java
+++ src/main/java/Lmbd.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Lmbd {
   public static void main(String[] args) {
     String logo = "██╗     ███╗   ███╗██████╗ ██████╗ \n"
@@ -9,7 +12,7 @@ public class Lmbd {
     System.out.println(logo);
 
     greet();
-    exit();
+    listen();
   }
 
   private static void print_horizontal_bar() {
@@ -21,9 +24,30 @@ public class Lmbd {
     System.out.println("Hello! I'm LMBD. What can I do for you?");
   }
 
+  private static void listen() {
+    Scanner sc = new Scanner(System.in);
+    while (true) {
+      print_horizontal_bar();
+      System.out.print("$ ");
+      String[] in = sc.nextLine().split(" ");
+      handle_command(in[0], Arrays.copyOfRange(in, 1, in.length));
+    }
+  }
+
+  private static void handle_command(String cmd, String[] args) {
+    switch (cmd) {
+      case "bye":
+        exit();
+      default:
+        print_horizontal_bar();
+        System.out.println(cmd + " " + String.join(" ", args));
+    }
+  }
+
   private static void exit() {
     print_horizontal_bar();
     System.out.println("Bye. Hope to see you again soon!");
     print_horizontal_bar();
+    System.exit(0);
   }
 }
